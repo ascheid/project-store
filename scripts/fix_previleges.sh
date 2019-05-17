918b8da78e1162eec96fb5dd814a13619ae1b57f
Alvaro Scheid <ascheid@gmail.com>
2019-05-17 15:53:51 -0400

    pipeline files

---
+++ scripts/fix_previleges.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod +x /home/ubuntu/project-store-0.0.1-SNAPSHOT.jar
+chmod +x /home/ubuntu/start_server.sh
+chmod +x /home/ubuntu/stop_server.sh
---
+++ scripts/start_server.sh
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=dev /home/ubuntu/project-store-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
